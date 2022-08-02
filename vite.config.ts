81c7e41c320f6d9f4051dd937538521e407e3d99
BetaCatPro <zj20162325@163.com>
2022-08-02 21:13:23 +0800

    chore: 配置路径别名@

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+// import which is supported in v16.0.0+ and v14.18.0+. v15 does not support this.
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [vue()],
+    resolve: {
+        // 配置路径别名
+        alias: {
+            '@': resolve(__dirname, './src')
+        }
+    }
 })
