2e371984f3c4ccdf8f902621a3cf11ce7fe81f87
BetaCatPro <zj20162325@163.com>
2022-08-02 21:14:14 +0800

    feat: 路由初始化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,7 @@ module.exports = {
                 }
             }
         ],
-        "@typescript-eslint/no-explicit-any": ["off"]
+        "@typescript-eslint/no-explicit-any": ["off"],
+        "vue/multi-word-component-names":"off",
     }
 }
