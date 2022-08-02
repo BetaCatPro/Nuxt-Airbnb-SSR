e23cb0f85c9c6cadb66048000a74d7cbd372e377
BetaCatPro <zj20162325@163.com>
2022-08-02 23:41:41 +0800

    build: 初始化element-plus及sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
 // import which is supported in v16.0.0+ and v14.18.0+. v15 does not support this.
 import { resolve } from 'path'
+// import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
+        })
+    ],
     resolve: {
         // 配置路径别名
         alias: {
             '@': resolve(__dirname, './src')
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@import "src/styles/index.scss";'
+            }
         }
     }
 })
