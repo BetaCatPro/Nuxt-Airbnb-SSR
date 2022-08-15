854888e94adaa6c816862dfa1f2801810e0baa5b
BetaCatPro <zj20162325@163.com>
2022-08-15 17:38:45 +0800

    feat: 添加tailwind支持

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: 'Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", PingFang-SC, "Hiragino Sans GB", 微软雅黑, "Microsoft YaHei", "Heiti SC", sans-serif',
-                heiti:
-                    'Circular, PingFang-SC, "Hiragino Sans GB", 微软雅黑, "Microsoft YaHei", "Heiti SC"',
+                heiti: 'Circular, PingFang-SC, "Hiragino Sans GB", 微软雅黑, "Microsoft YaHei", "Heiti SC"',
                 airmoji: 'Airmoji'
             },
             fontSize: {
@@ -42,4 +41,4 @@ module.exports = {
         }
     },
     plugins: [require('@tailwindcss/line-clamp')]
-};
+}
